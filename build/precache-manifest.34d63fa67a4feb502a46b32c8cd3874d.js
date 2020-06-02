ca50ca0208868ebb1b0a44206c873b3f58227aa7
Origho Precious <53418429+Origho-precious@users.noreply.github.com>
2020-06-03 00:52:13 +0100

    Added build

---
+++ build/precache-manifest.34d63fa67a4feb502a46b32c8cd3874d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1e8d9b24db5633109cade290fa7692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e40db5e5068bd36b7e3c",
+    "url": "/static/css/main.1b374e36.chunk.css"
+  },
+  {
+    "revision": "9f2663ddb3ef11f851aa",
+    "url": "/static/js/2.37762fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37762fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40db5e5068bd36b7e3c",
+    "url": "/static/js/main.10ddb037.chunk.js"
+  },
+  {
+    "revision": "c4aae0ede7172aa9af39",
+    "url": "/static/js/runtime-main.70b2170d.js"
+  }
+]);
